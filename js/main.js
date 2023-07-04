f0127e64e89d12b71dc1e076c7415c1f8280feab
Alexander Belov <114015310+atomtim@users.noreply.github.com>
2023-07-04 20:11:04 +0300

    Добавлена ссылка на версию 3189

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // List of Carrot Carrot versions
-const docsifyVersions = [{ folder: "4062", label: "rev 4062", default: true }];
+const docsifyVersions = [{ folder: "4062", label: "rev 4062", default: true }{ folder: "3189", label: "rev 3189", default: false }];
 
 // init docsify object
 window.$docsify = {
